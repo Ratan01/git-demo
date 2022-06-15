da9b6a191d79ae772202a09d0a463f7185d4729f
Rudra01 <ratanrudra010@gmail.com>
2022-06-15 10:11:06 +0530

    changes made

--- src/com/cg/gitDemo/Greet.java
+++ src/com/cg/gitDemo/Greet.java
@@ -4,5 +4,6 @@ public class Greet {
 	public static void main(String[] args) {
 		System.out.println("welcome to git");
 		System.out.println("welcome to cg world");
+		System.out.println("git demo practical");
 	}
 }
